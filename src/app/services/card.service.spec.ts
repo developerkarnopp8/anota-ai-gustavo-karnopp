d84a10ff462476a548816ef295a1265e6ab13824
developerkarnopp8 <developer.karnopp@gmail.com>
2025-04-29 12:08:24 -0300

    test: enhance CardService tests with proper setup and mock data

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CardService } from './card.service';
+import { Card } from '../models/card/card.model';
 
 describe('CardService', () => {
   let service: CardService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CardService],
+    });
+
     service = TestBed.inject(CardService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve buscar uma lista de cards (GET)', () => {
+    const mockCards: Card[] = [
+      { 
+        id: 1, 
+        title: 'Card 1', 
+        description: 'Descrição 1', 
+        img: 'img1.png', 
+        type: '1' 
+      },
+      { 
+        id: 2, 
+        title: 'Card 2', 
+        description: 'Descrição 2', 
+        img: 'img2.png', 
+        type: '2' 
+      }
+    ];
+
+    service.getCards().subscribe(cards => {
+      expect(cards.length).toBe(2);
+      expect(cards).toEqual(mockCards);
+    });
+
+    const req = httpMock.expectOne(service.apiUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockCards);
   });
 });
