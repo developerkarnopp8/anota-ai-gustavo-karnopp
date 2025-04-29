bcbb215b2dfce6a8649cd01724891a3962278b17
developerkarnopp8 <developer.karnopp@gmail.com>
2025-04-29 09:58:41 -0300

    feat: enhance card list and card components with search functionality and improved styling

--- src/app/features/components/card-list/card-list.component.ts
+++ src/app/features/components/card-list/card-list.component.ts
@@ -1,31 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { CardComponent } from "../card/card.component";
 import { CardService } from '../../../services/card.service';
+import { Card } from '../../../models/card/card.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
-  imports: [CardComponent],
+  standalone: true,
+  imports: [CardComponent, CommonModule, FormsModule],
   templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.scss'
+  styleUrl: './card-list.component.scss',
 })
-export class CardListComponent {
+export class CardListComponent implements OnInit {
 
-  constructor(
-    private cardService : CardService
-  ){}
+  cards = signal<Card[]>([]);
+  searchTerm = signal('');
 
-  searchTerm = '';
+  constructor(private cardService: CardService) {}
 
-  // get filteredCards() {
-  //   return this.cards.filter(card =>
-  //     card.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-  //     card.description.toLowerCase().includes(this.searchTerm.toLowerCase())
-  //   );
-  // }
-  
+  ngOnInit() {
+    this.cardService.getCards().subscribe(data => {
+      this.cards.set(data);
+    });
+  }
 
-  // removeCard(id: number) {
-  //   this.cards = this.cards.filter(card => card.id !== id);
-  // }
+  removeCard(id: number) {
+    const newCart = this.cards().filter(card => card.id !== id);
+    this.cards.set(newCart);
+  }
 
+  filteredCards = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+
+    if (!term) {
+      return this.cards();
+    }
+    
+    return this.cards().filter(card =>
+      card.title.toLowerCase().includes(term) ||
+      card.description.toLowerCase().includes(term)
+    );
+  });
+
+  onInputChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.searchTerm.set(input.value);
+  }
+
+  onSearchClick() {
+    console.log('Busca acionada manualmente:', this.searchTerm());
+  }
+
+  clearSearch() {
+    this.searchTerm.set('');
+  }
 }
--- src/app/features/components/card/card.component.ts
+++ src/app/features/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Card } from '../../../models/card/card.model';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  close: string = 'https://githubanotaai.github.io/frontend-interview-mock-data/assets/close.svg'
+  @Input() card!: Card;
+  @Output() delete = new EventEmitter<number>();
+
+  get typeName(): string {
+    switch (this.card.type) {
+      case 1: return 'Paisagem';
+      case 2: return 'Flor';
+      case 3: return 'Pizza';
+      default: return 'Desconhecido';
+    }
+  }
 
 }
